77ae8c501c712a8f73b6926a584ab703733c410a
William Ross <bill_87_2@yahoo.com>
2025-03-25 19:40:21 -0500

    Commit initial working Card class

---
+++ lib/card.rb
@@ -0,0 +1,13 @@
+# Card class must have suit, value, rank
+
+class Card
+    attr_reader :suit, :value, :rank
+
+    def initialize(suit, value, rank)
+        @suit = suit
+        @value = value
+        @rank = rank
+    end
+
+end
+
